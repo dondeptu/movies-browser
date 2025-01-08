edce7724b6f66e0b2affa428c44dd499dd77d386
Rafał Olewicz <namerafal75@gmail.com>
2025-01-08 19:16:37 +0100

    Grid style added

--- src/components/commonStyle/styled.js
+++ src/components/commonStyle/styled.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-export const TileContent = styled.div``;
+export const TileContent = styled.div`
+    display: grid;
+    grid-column-start: 2;
+    grid-row-end: 1;
+`;
 
 export const TileTitle = styled.header`
     font-weight: 600;
