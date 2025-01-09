b5f8efe2d82a38924b2b8e27b83fe6c2c976b89f
Donata Deptula <dondeptu@gmail.com>
2025-01-10 00:15:39 +0100

    added left and right margin to Content of MovieList

--- src/features/MovieList/styled.js
+++ src/features/MovieList/styled.js
@@ -4,7 +4,7 @@ export const Content = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(324px, 1fr));
     gap: 24px;
-    margin: 24px 0;
+    margin: 20px 16px;
     justify-content: center;
     justify-items: center;
 
