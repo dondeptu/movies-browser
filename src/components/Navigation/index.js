e3b00da0fa836f6f01905b4a18e70a4c79f0de5b
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-01-09 15:40:37 +0100

    Add Navigation component with Movies and People tabs, active tab functionality, and search bar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,30 +1,45 @@
+import React, { useState } from "react";
+
 import {
   NavigationStyled,
   Wrapper,
-  LogoIcon,
+  NavGroup,
   Logo,
+  LogoIcon,
   LogoTitle,
   ListStyled,
   NavItem,
+  SearchBarStyled,
+  SearchBarIcon,
+  Input,
 } from "./styled";
 
 export const Navigation = () => {
+  const [activeTab, setActiveTab] = useState("Movies");
   return (
     <NavigationStyled>
       <Wrapper>
-        <Logo>
-          <LogoIcon />
-          <LogoTitle>Movies Browser</LogoTitle>
-        </Logo>
+        <NavGroup>
+          <Logo>
+            <LogoIcon />
+            <LogoTitle>Movies Browser</LogoTitle>
+          </Logo>
+          <ListStyled>
+            <li>
+              <NavItem isActive={activeTab === "Movies"}
+              onClick={() => setActiveTab("Movies")}>Movies</NavItem>
+            </li>
+            <li>
+              <NavItem isActive={activeTab === "People"}
+              onClick={() => setActiveTab("People")}>People</NavItem>
+            </li>
+          </ListStyled>
+        </NavGroup>
 
-        <ListStyled>
-          <li>
-            <NavItem>Movies</NavItem>
-          </li>
-          <li>
-            <NavItem>People</NavItem>
-          </li>
-        </ListStyled>
+        <SearchBarStyled>
+          <SearchBarIcon />
+          <Input type="text" placeholder="Search for movies..." />
+        </SearchBarStyled>
       </Wrapper>
     </NavigationStyled>
   );
