c6c7ec7f3500458f2e28cb9332e945053538cbd3
Donata Deptula <dondeptu@gmail.com>
2025-01-10 01:37:16 +0100

    code cleaning

--- src/common/MovieTile/Genre/index.js
+++ src/common/MovieTile/Genre/index.js
@@ -1,7 +1,6 @@
 import { Container, GenreItem } from "../Genre/styled";
 
 export const Genre = ({ genres }) => {
-    console.log('Genres:', genres);
     return (
         <Container>
             {genres.map(({ tag }, index) => (
