37f0829876399509cc81930a8bef1b316837d1d5
Donata Deptula <dondeptu@gmail.com>
2025-01-10 16:51:13 +0100

    adjusted margins and line heights, also optimized for mobile widths

--- src/features/MovieList/styled.js
+++ src/features/MovieList/styled.js
@@ -4,13 +4,13 @@ export const Content = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(324px, 1fr));
     gap: 24px;
-    margin: 20px 0;
+    margin: 24px 0;
     justify-content: center;
     justify-items: center;
 
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         grid-template-columns: minmax(288px, 1fr);
         gap: 16px;
-        margin: 12px 0;
+        margin: 16px 0;
     }
 `;
