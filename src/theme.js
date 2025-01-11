cf063809d9b302ead70a3ded6fa204e920f9788c
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-01-11 21:31:45 +0100

    add breakpoint for mobileMax

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,15 @@
 export const theme = {
-    color: {
-        white: '#FFFFFF',
-        whisper: '#F5F5FA',
-        mystic: '#E4E6F0',
-        candlelight: '#FCD420',
-        stormGray: '#74788B',
-        waterloo: '#7E839A',
-        black: '#18181B',
-    },
+  color: {
+    white: "#FFFFFF",
+    whisper: "#F5F5FA",
+    mystic: "#E4E6F0",
+    candlelight: "#FCD420",
+    stormGray: "#74788B",
+    waterloo: "#7E839A",
+    black: "#18181B",
+  },
+
+  breakpoint: {
+    mobileMax: 767,
+  },
 };
