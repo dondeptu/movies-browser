1afffc06a9da5f3cc0d0e20b8da5aa334141c7b7
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-01-12 11:10:53 +0100

    replace previous colors and reorder from lightest to darkest

--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,16 @@
 export const theme = {
-    color: {
-        white: '#FFFFFF',
-        whisper: '#F5F5FA',
-        mystic: '#E4E6F0',
-        candlelight: '#FCD420',
-        stormGray: '#74788B',
-        waterloo: '#7E839A',
-        black: '#18181B',
-    },
-    fontWeight: {
-        regular: 400,
-        medium: 500,
-        semiBold: 600,
-        bold: 700
-    },
+  color: {
+    white: "#FFFFFF",
+    lightblue: "#D6E4FF",
+    yellow: "#FCD420",
+    lightgrey: "#F5F5FA",
+    blue: "#0044CC",
+    grey: "#E4E6F0",
+    darkergrey: "#7E839A",
+    black: "#18181B",
+  },
 
-    breakpoint: {
-        mobileMax: 767,
-      },
+  breakpoint: {
+    mobileMax: 767,
+  },
 };
