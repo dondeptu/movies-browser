b305d820551c068bee0c1cceec56f583a9c023ef
Rafał Olewicz <namerafal75@gmail.com>
2025-01-16 13:09:27 +0100

    Styles settings for PersonTile have been updated

--- src/common/PersonTile/styled.js
+++ src/common/PersonTile/styled.js
@@ -1,30 +1,29 @@
 import styled from "styled-components";
 
-export const PersonTileWrapper = styled.div`
+export const PersonTileWrapper = styled.a`
     background:  ${({ theme }) => theme.color.white};
+    box-shadow: ${({ theme }) => theme.shadows.elevation};
     width: 208px;
     height: 329px;
     padding: 16px;
-    box-shadow: 0px 4px 12px 0px #BAC7D580;
+    border-radius: 5px;
+    text-decoration: none;
     display: grid;
-    grid-template-rows: auto 1fr;
-    grid-gap: 12px;
-    transition: 0.5s;
+    grid-template-columns: auto;
+    grid-template-rows: auto 1fr;    
+    cursor: pointer;
+    transition: 0.3s;
     
-    &:hover{
-        /* opacity: 0.8; */
+    &:hover{        
         transform: scale(1.03);
     }
 
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-        /* width: 136px;
-        height: 245px; */
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {        
         height: fit-content;       
         width: fit-content;
         padding: 8px;
         grid-row-gap: 8px;
-    }
-    
+    }    
 `;
 
 export const PersonImage = styled.img`
@@ -35,23 +34,36 @@ export const PersonImage = styled.img`
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         width: 120px;
-        height: 165px;        
+        height: 176px;        
     }
 `;
 
-export const PersonTileContent = styled.div`    
-    text-align: center;
-`;
-
-export const PersonName = styled.div`
+export const PersonName = styled.h2`    
     font-weight: 500;
     font-size: 22px;
-    /* line-height:1.6; */
-    margin: 0px 0px 8px;
+    line-height: 1.3;
+    text-align: center;
+    margin: 12px 0px 8px;
+    overflow-wrap: anywhere;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 14px;
+        margin: 0px;        
+    }
 `;
 
-export const PersonSubtitle = styled.div`
+export const PersonSubtitle = styled.h3`
     color: ${({ theme }) => theme.color.darkergrey};
-    font-size: 18px;
-    line-height: 1.8;
+    font-size: 18px;    
+    line-height: 1.5;
+    font-weight: normal;
+    margin: -16px 0px 0px;
+    margin: 0px;
+    text-align: center;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-weight: 400;
+        font-size: 13px;
+        margin: 0px;
+    }
 `;
