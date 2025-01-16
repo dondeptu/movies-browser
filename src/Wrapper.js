a2dbdfbc06a1bcaf4c1706d7b8dff1d51c5a6c95
Rafał Olewicz <namerafal75@gmail.com>
2025-01-16 12:58:37 +0100

    Settings and names of elements in the persons section have been updated

--- src/Wrapper.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.main`
-    max-width: 1368px;
-    margin: 0 auto;
-    overflow-wrap: anywhere;
-`;
--- src/common/PersonsSection/index.js
+++ src/common/PersonsSection/index.js
@@ -1,12 +1,10 @@
-import { SectionPerson, Section, TitleSection } from "./styled.js";
+import { SectionPerson, PersonsHeader } from "./styled.js";
 
 const PersonsSection = ({ title, body }) => (
-  <Section>
-    <TitleSection>{title}</TitleSection>
-    <SectionPerson>
-      {body}
-    </SectionPerson>
-  </Section>
+  <>
+    <PersonsHeader>{title}</PersonsHeader>
+    <SectionPerson>{body}</SectionPerson>
+  </>
 );
 
 export default PersonsSection;
