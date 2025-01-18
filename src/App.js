1a08467fbfb87d73bdd2ccf374ebab52d1e90302
Rafał Olewicz <namerafal75@gmail.com>
2025-01-18 17:08:17 +0100

    Temporary version of the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Navigation } from './common/Navigation';
+// import MovieList from './features/MovieList';
 import MoviePage from './features/MoviePage';
 
 export const App = () => (
   <>
     <Navigation />
     <MoviePage />
+    {/* <MovieList /> */}
   </>
 );
