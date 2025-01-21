09fdbbece2fbbdc6d0f0963f2f622134bb5e3dd5
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-01-21 19:30:13 +0100

    implement Redux-Saga for fetching popular people

---
+++ src/features/People/peopleSaga.js
@@ -0,0 +1,27 @@
+import { call, delay, put, takeEvery, select } from "redux-saga/effects";
+import { fetchPopularPeople } from "./fetchPeopleData";
+import {
+  startFetch,
+  fetchPeople,
+  fetchPeopleError,
+  fetchPeopleSuccess,
+} from "./peopleSlice";
+import { selectCurrentPage } from "../../common/Pagination/paginationSlice";
+
+function* fetchPeopleData() {
+  try {
+    const page = yield select(selectCurrentPage);
+    const { results, total_pages, total_results } = yield call(fetchPopularPeople, page);
+    yield put(
+      fetchPeople({ results, total_pages, total_results })
+    );
+    yield delay(1000);
+    yield put(fetchPeopleSuccess());
+  } catch (error) {
+    yield put(fetchPeopleError(`Error fetching people: ${error.message}`));
+  }
+}
+
+export function* peopleSaga() {
+  yield takeEvery(startFetch, fetchPeopleData);
+}
