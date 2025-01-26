2e90f995adf16fe8179cd9c5180bf058f293e836
Donata Deptula <dondeptu@gmail.com>
2025-01-26 22:29:25 +0100

    Implement api call for movie details by ID.

--- src/features/Movies/MoviePage/index.js
+++ src/features/Movies/MoviePage/index.js
@@ -1,18 +1,37 @@
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useEffect } from "react";
 import { MovieDetailsTile } from "../../../common/MovieDetailsTile";
 import { MainContent } from "../../../common/MainContent";
 import { Wrapper } from "../../../Wrapper";
+import { fetchMovieDetails, selectMovieDetails } from "../movieSlice";
+
 
 function MoviePage() {
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const movieDetails = useSelector(selectMovieDetails);
+
+    useEffect(() => {
+        dispatch(fetchMovieDetails(id));
+    }, [dispatch, id]);
+
+    console.log(movieDetails);
+
     return (
         <Wrapper>
             <MainContent
                 mainHeader=""
                 body={
-                    <MovieDetailsTile />
+                    movieDetails ? (
+                        <MovieDetailsTile  />
+                    ) : (
+                        <p>No details available</p>
+                    )
                 }
             />
         </Wrapper>
     );
 }
-
 export default MoviePage;
--- src/features/Movies/movieSlice.js
+++ src/features/Movies/movieSlice.js
@@ -4,23 +4,19 @@ const movieSlice = createSlice({
   name: "movies",
   initialState: {
     movies: {
-      movies: [],
+      results: [],
       total_results: 0,
     },
+    movieDetails: {},
     loading: false,
     error: null,
-    // id: "",
   },
   reducers: {
     startFetch: (state) => {
       state.loading = true;
     },
-    // setId: (state, { payload: id }) => {
-    //   state.id = id;
-    //   state.loading = true;
-    // },
     fetchMovies: (state, { payload: popularMovies }) => {
-      state.movies.movies = popularMovies.results;
+      state.movies.results = popularMovies.results;
       state.movies.total_pages = 500;
       state.movies.total_results = popularMovies.total_results;
       state.loading = false;
@@ -33,6 +29,10 @@ const movieSlice = createSlice({
       state.loading = false;
       state.error = errorMessage;
     },
+    setMovieDetails: (state, { payload: movieDetails }) => {
+      state.movieDetails = movieDetails;
+    },
+    fetchMovieDetails: () => { },
   },
 });
 
@@ -40,8 +40,9 @@ export const selectMoviesState = (state) => state.movies;
 export const selectMovies = (state) => selectMoviesState(state).movies;
 export const selectLoading = (state) => selectMoviesState(state).loading;
 export const selectError = (state) => selectMoviesState(state).error;
+export const selectMovieDetails = (state) => selectMoviesState(state).movieDetails;
 
-export const { fetchMovies, startFetch, fetchMovieSuccess, fetchMovieError } =
+export const { fetchMovies, startFetch, fetchMovieSuccess, fetchMovieError, setMovieDetails, fetchMovieDetails } =
   movieSlice.actions;
 
 export default movieSlice.reducer;
