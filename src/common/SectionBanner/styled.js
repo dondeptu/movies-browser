11650b9fafd481e42db972c4522b421cf09e97cb
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-01-26 17:46:44 +0100

    replace section with header in Wrapper component

--- src/common/SectionBanner/styled.js
+++ src/common/SectionBanner/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.section`
+export const Wrapper = styled.header`
   padding-bottom: 8px;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
