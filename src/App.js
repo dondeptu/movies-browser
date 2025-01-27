55349532178c318e16d2b76393d3eda26cf07314
Rafał Olewicz <namerafal75@gmail.com>
2025-01-27 21:50:14 +0100

    Removal of temporary render of the PeoplePage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import { Navigation } from "./common/Navigation";
 import MovieList from "./features/Movies/MovieList";
 import MoviePage from "./features/Movies/MoviePage";
 import PeopleList from "./features/People/PeopleList";
-import PeoplePage from "./features/People/PeoplePage";
 import { Pagination } from "./common/Pagination";
 import { toMovieList, toMoviePage, toPeople } from "./routes";
 
@@ -18,8 +17,6 @@ export const App = () => (
   <Provider store={store}>
     <HashRouter>
       <Navigation />
-      {/* Tymczasowe włączenie  */}
-      <PeoplePage />
       <Switch>
         <Route path={toMoviePage()}>
           <MoviePage />
