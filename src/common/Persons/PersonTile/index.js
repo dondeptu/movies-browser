fde4a4e41ad339e2fb5da34cb9ff0affb0b7595a
Rafał Olewicz <namerafal75@gmail.com>
2025-01-28 00:01:55 +0100

    The font was corrected after the description jump

--- src/common/Persons/PersonTile/index.js
+++ src/common/Persons/PersonTile/index.js
@@ -1,13 +1,12 @@
 import { ContentPerson, PersonImage, PersonName, PersonSubtitle, PersonTileWrapper } from "./styled";
 import posterLiu from "../../../images/posterLiu.png";
-import { Content } from "../../../features/Movies/MovieList/styled";
 
 export const PersonTile = ({ showSubtitle = true }) => (
     <PersonTileWrapper>
         <PersonImage src={posterLiu} alt="poster of Lui" />
         <ContentPerson>
-        <PersonName>Liu Yifei Long Name Long name</PersonName>
-        {showSubtitle && <PersonSubtitle>Mulan</PersonSubtitle>}
+            <PersonName>Liu Yifei Long Name Long name</PersonName>
+            {showSubtitle && <PersonSubtitle>Mulan</PersonSubtitle>}
         </ContentPerson>
     </PersonTileWrapper>
 );
