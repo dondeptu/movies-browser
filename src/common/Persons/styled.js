75d9639659673bff56626167036a677a90d81181
Rafał Olewicz <namerafal75@gmail.com>
2025-01-27 09:15:43 +0100

    Updated margin-bottom to PersonsContent

--- src/common/Persons/styled.js
+++ src/common/Persons/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const PersonsContent = styled.div`
     margin-top: 32px;
+    margin-bottom: 64px;
     display: grid; 
     grid-template-columns: repeat(auto-fill, minmax(208px, 1fr));
     justify-items: center;
@@ -11,5 +12,6 @@ export const PersonsContent = styled.div`
         grid-template-columns: repeat(auto-fill, minmax(136px, 1fr));
         grid-gap: 16px;
         margin-top: 12px;
+        margin-bottom: 0px;
     }
 `;
