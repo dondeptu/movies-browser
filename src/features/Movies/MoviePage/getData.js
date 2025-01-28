a971c256cff596ac09fe74408b9891169b050eab
Donata Deptula <dondeptu@gmail.com>
2025-01-29 00:56:18 +0100

    Fixed redundant query parameter delimiter in API call

--- src/features/Movies/MoviePage/getData.js
+++ src/features/Movies/MoviePage/getData.js
@@ -3,7 +3,7 @@ import { apiKey } from "../../../api";
 
 export const getMovieDetails = async (movieId) => {
     try {
-        const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?language=en-US&&api_key=${apiKey}`);
+        const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?language=en-US&api_key=${apiKey}`);
         return response.data;
     } catch (error) {
         console.error(`Failed to fetch movie details: ${error.message}`);
