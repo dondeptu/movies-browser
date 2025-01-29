374735a46bc27236b8e305041072f867092ca1e2
Donata Deptula <dondeptu@gmail.com>
2025-01-29 01:18:51 +0100

    Add scrollToTop component to reset scroll position on route change

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ import MoviePage from "./features/Movies/MoviePage";
 import PeopleList from "./features/People/PeopleList";
 import { Pagination } from "./common/Pagination";
 import { toMovieList, toMoviePage, toPeople } from "./routes";
+import ScrollToTop from "./common/ScrollToTop";
 
 export const App = () => (
   <Provider store={store}>
     <HashRouter>
-      <Navigation />     
+      <ScrollToTop />
+      <Navigation />
       <Switch>
         <Route path={toMoviePage()}>
           <MoviePage />
