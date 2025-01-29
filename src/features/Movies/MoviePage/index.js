0cdf18c7f5513b4fd158d73c412a9a124f7adbd0
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-01-29 14:22:16 +0100

    Add loading and error handling for movie page

--- src/features/Movies/MoviePage/index.js
+++ src/features/Movies/MoviePage/index.js
@@ -9,76 +9,90 @@ import { MainContent } from "../../../common/MainContent";
 import { MovieBanner } from "../../../common/tiles/MovieDetailsTile/banner/MovieBanner";
 import { SectionBanner } from "../../../common/tiles/MovieDetailsTile/banner/SectionBanner";
 import { Wrapper } from "../../../Wrapper";
-import { fetchMovieDetails, selectMovieDetails } from "../movieSlice";
-
+import {
+  fetchMovieDetails,
+  selectMovieDetails,
+  selectMoviesState,
+} from "../movieSlice";
+import { Loading } from "../../../common/Loading";
+import { Error } from "../../../common/Error";
 
 function MoviePage() {
-    const dispatch = useDispatch();
-    const { id } = useParams();
-    const movieDetails = useSelector(selectMovieDetails);
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const movieDetails = useSelector(selectMovieDetails);
+  const { loading, error } = useSelector(selectMoviesState);
 
-    useEffect(() => {
-        dispatch(fetchMovieDetails(id));
-    }, [dispatch, id]);
+  useEffect(() => {
+    dispatch(fetchMovieDetails(id));
+  }, [dispatch, id]);
 
-    return (
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Error />
+      ) : (
         <>
-            <SectionBanner
-                body={
-                    <MovieBanner
-                        backdropPath={movieDetails.backdrop_path}
-                        title={movieDetails.title}
-                        voteAverage={movieDetails.vote_average}
-                        voteCount={movieDetails.vote_count}
-                    />
-                }
+          <SectionBanner
+            body={
+              <MovieBanner
+                backdropPath={movieDetails.backdrop_path}
+                title={movieDetails.title}
+                voteAverage={movieDetails.vote_average}
+                voteCount={movieDetails.vote_count}
+              />
+            }
+          />
+          <Wrapper>
+            <MainContent
+              mainHeader=""
+              body={
+                movieDetails ? (
+                  <MovieDetailsTile
+                    poster_path={movieDetails.poster_path}
+                    title={movieDetails.title}
+                    production_countries={movieDetails.production_countries}
+                    release_date={movieDetails.release_date}
+                    vote_average={movieDetails.vote_average}
+                    vote_count={movieDetails.vote_count}
+                    overview={movieDetails.overview}
+                  />
+                ) : (
+                  <p>No details available</p>
+                )
+              }
             />
-            <Wrapper>
-                <MainContent
-                    mainHeader=""
-                    body={
-                        movieDetails ? (
-                            <MovieDetailsTile
-                                poster_path={movieDetails.poster_path}
-                                title={movieDetails.title}
-                                production_countries={movieDetails.production_countries}
-                                release_date={movieDetails.release_date}
-                                vote_average={movieDetails.vote_average}
-                                vote_count={movieDetails.vote_count}
-                                overview={movieDetails.overview}
-                            />
-                        ) : (
-                            <p>No details available</p>
-                        )
-                    }
-                />
-                <MainContent
-                    mainHeader="Cast"
-                    body={
-                        <PersonsContent>
-                            <PersonTile />
-                            <PersonTile />
-                            <PersonTile />
-                            <PersonTile />
-                            <PersonTile />
-                            <PersonTile />
-                            <PersonTile />
-                            <PersonTile />
-                        </PersonsContent>
-                    }
-                />
-                < MainContent
-                    mainHeader="Crew"
-                    body={
-                        <PersonsContent>
-                            <PersonTile />
-                            <PersonTile />
-                            <PersonTile />
-                        </PersonsContent>
-                    }
-                />
-            </Wrapper >
+            <MainContent
+              mainHeader="Cast"
+              body={
+                <PersonsContent>
+                  <PersonTile />
+                  <PersonTile />
+                  <PersonTile />
+                  <PersonTile />
+                  <PersonTile />
+                  <PersonTile />
+                  <PersonTile />
+                  <PersonTile />
+                </PersonsContent>
+              }
+            />
+            <MainContent
+              mainHeader="Crew"
+              body={
+                <PersonsContent>
+                  <PersonTile />
+                  <PersonTile />
+                  <PersonTile />
+                </PersonsContent>
+              }
+            />
+          </Wrapper>
         </>
-    );
+      )}
+    </>
+  );
 }
 export default MoviePage;
