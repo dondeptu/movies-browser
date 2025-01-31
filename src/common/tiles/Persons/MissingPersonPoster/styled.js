a04c51384dc245eb277b659d34593ff3b286d323
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-01-31 17:11:01 +0100

    style missingPersonPoster component

---
+++ src/common/tiles/Persons/MissingPersonPoster/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { ReactComponent as missingPerson } from "../../../../images/iconMissingPerson.svg";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: auto;
+  border-radius: 5px;
+  aspect-ratio: 2/3;
+  background-color: ${({ theme }) => theme.color.postergrey};
+`;
+
+export const StyledMissingPerson = styled(missingPerson)`
+  width: 72px;
+  height: auto;
+
+  path {
+    stroke-width: 1.5px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    width: 48px;
+  }
+`;
