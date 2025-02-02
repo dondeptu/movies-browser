a9d308afc809719874bb50849712519d051775d1
Rafał Olewicz <namerafal75@gmail.com>
2025-02-02 12:30:44 +0100

    To Description was added PersonDescription

--- src/common/tiles/MovieDetailsTile/Description/index.js
+++ src/common/tiles/MovieDetailsTile/Description/index.js
@@ -1,8 +1,15 @@
-import { TileDescription } from "./styled";
+import { PersonDescriptionWrapper, TileDescription } from "./styled";
 
 export const MovieDescription = ({ movieDescription }) => {
 
     return (
         <TileDescription>{movieDescription.description || "No description available."}</TileDescription>
+    )
+};
+
+export const PersonDescription = ({ personDescription }) => {
+
+    return (
+        <PersonDescriptionWrapper>{personDescription || "No description available."}</PersonDescriptionWrapper>
     )
 };
