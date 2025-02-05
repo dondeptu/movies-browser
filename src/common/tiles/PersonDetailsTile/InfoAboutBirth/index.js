8215ab5c6fad7c938f8724f31ac9168550227d04
Rafał Olewicz <namerafal75@gmail.com>
2025-02-05 22:59:52 +0100

    Adding a dynamic change of "Birth" name for mobile and "Date of birth" for desktop and dependence about birthday information is or not.

--- src/common/tiles/PersonDetailsTile/InfoAboutBirth/index.js
+++ src/common/tiles/PersonDetailsTile/InfoAboutBirth/index.js
@@ -1,19 +1,34 @@
+import { useEffect, useState } from "react";
+import { theme } from "../../../../theme";
 import { Wrapper, Content, NameItemDisplay, ContentItem } from "../../MovieDetailsTile/ProductionInfo/styled";
 
 export const InfoAboutBirth = ({ info, personDetailsSize }) => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= theme.breakpoint.mobileMax);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= theme.breakpoint.mobileMax);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
 
     return (
         <Wrapper>
             <Content>
-                <NameItemDisplay>Birth:</NameItemDisplay>
+                <NameItemDisplay>
+                    {isMobile ? 'Birth:' : 'Date of birth:'}
+                </NameItemDisplay>
                 <ContentItem personDetailsSize={personDetailsSize} >
-                    {info.dateOfBirth}
+                    {info.dateOfBirth || "Unknown"}
                 </ContentItem>
             </Content>
             <Content>
                 <NameItemDisplay>Place of birth:</NameItemDisplay>
                 <ContentItem personDetailsSize={personDetailsSize}>
-                    {info.placeOfBirth}
+                    {info.placeOfBirth || "Unknown"}
                 </ContentItem>
             </Content>
         </Wrapper>
