6db1fdbc8b01c8ae874ab1d1ca800de451865855
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-06 17:03:03 +0100

    optimize scroller to avoid page moving

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.main`
-    max-width: 1400px;
-    margin: 0 auto;
-    overflow-wrap: anywhere;
-    display: flex;
-    flex-direction: column;
-    min-height: calc(100vh - 94px);
+  max-width: 1400px;
+  margin: 0 auto;
+  overflow-wrap: anywhere;
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - 94px);
+  overflow-y: auto;
 `;
