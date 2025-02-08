d8101e090f60a923157a5f0cb2e2038dfd37e969
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-08 21:47:55 +0100

    add loading and error handling

--- src/features/People/PeoplePage/index.js
+++ src/features/People/PeoplePage/index.js
@@ -6,118 +6,158 @@ import { Section } from "../../../common/Section";
 import { Content } from "../../Movies/MovieList/styled";
 import { PersonDetailsTile } from "../../../common/tiles/PersonDetailsTile";
 import { MovieTile } from "../../../common/tiles/MovieTile";
-import { fetchPeopleDetails, selectPeopleDetails } from "../peopleSlice";
+import {
+  fetchPeopleDetails,
+  selectPeopleDetails,
+  selectPeopleState,
+} from "../peopleSlice";
 import { Article } from "../../../common/Article";
 import { useIsMobile } from "../../../useIsMobile";
-import { selectCast, selectCrew } from "../../Movies/MoviePage/creditsSlice";
+import {
+  selectCast,
+  selectCastError,
+  selectCastLoading,
+  selectCrew,
+  selectCrewError,
+  selectCrewLoading,
+} from "../../Movies/MoviePage/creditsSlice";
+import { Loading } from "../../../common/Loading";
+import { Error } from "../../../common/Error";
 
 function PeoplePage() {
   const dispatch = useDispatch();
   const { id } = useParams();
   const peopleDetails = useSelector(selectPeopleDetails);
+  const { loading, error } = useSelector(selectPeopleState);
   const cast = useSelector(selectCast);
   const crew = useSelector(selectCrew);
+  const castError = useSelector(selectCastError);
+  const crewError = useSelector(selectCrewError);
+  const castLoading = useSelector(selectCastLoading);
+  const crewLoading = useSelector(selectCrewLoading);
 
   useEffect(() => {
     dispatch(fetchPeopleDetails(id));
   }, [dispatch, id]);
-  
+
   const isMobile = useIsMobile();
 
   return (
-    <Wrapper>
-      <Section
-        sectionHeader=""
-        body={
-          <>
-            {peopleDetails && (
-              <PersonDetailsTile
-                personDetailsSize
-                profilePath={peopleDetails.profile_path}
-                name={peopleDetails.name}
-                birthday={peopleDetails.birthday}
-                placeOfBirth={peopleDetails.place_of_birth}
-                biography={peopleDetails.biography}
-              />
-            )}
-            <Article
-              articleHeader="Movies - cast"
-              body={
-                <Content>
-                  {cast.length > 0 ? (
-                    cast.map(
-                      ({
-                        id,
-                        poster_path,
-                        title,
-                        release_date,
-                        genre_ids,
-                        vote_average,
-                        vote_count,
-                        character,
-                      }) => (
-                        <MovieTile
-                          key={id}
-                          id={id}
-                          posterPath={poster_path}
-                          title={title}
-                          castCharacter={isMobile ? undefined : character}
-                          releaseDate={
-                            isMobile ? release_date?.slice(0, 4) : release_date
-                          }
-                          genreIds={genre_ids}
-                          voteAverage={vote_average}
-                          voteCount={vote_count}
-                        />
+    <>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Error />
+      ) : (
+        <Wrapper>
+          <Section
+            sectionHeader=""
+            body={
+              <>
+                {peopleDetails && (
+                  <PersonDetailsTile
+                    personDetailsSize
+                    profilePath={peopleDetails.profile_path}
+                    name={peopleDetails.name}
+                    birthday={peopleDetails.birthday}
+                    placeOfBirth={peopleDetails.place_of_birth}
+                    biography={peopleDetails.biography}
+                  />
+                )}
+
+                {peopleDetails && (
+                  <Article
+                    articleHeader="Movies - cast"
+                    body={
+                      castLoading ? (
+                        <Loading />
+                      ) : castError || !cast || cast.length === 0 ? (
+                        <p>No movies available</p>
+                      ) : (
+                        <Content>
+                          {cast.map(
+                            ({
+                              id,
+                              poster_path,
+                              title,
+                              release_date,
+                              genre_ids,
+                              vote_average,
+                              vote_count,
+                              character,
+                            }) => (
+                              <MovieTile
+                                key={id}
+                                id={id}
+                                posterPath={poster_path}
+                                title={title}
+                                castCharacter={isMobile ? undefined : character}
+                                releaseDate={
+                                  isMobile
+                                    ? release_date?.slice(0, 4)
+                                    : release_date
+                                }
+                                genreIds={genre_ids}
+                                voteAverage={vote_average}
+                                voteCount={vote_count}
+                              />
+                            )
+                          )}
+                        </Content>
                       )
-                    )
-                  ) : (
-                    <p>No movies available</p>
-                  )}
-                </Content>
-              }
-            />
-            <Article
-              articleHeader="Movies - crew"
-              body={
-                <Content>
-                  {crew.length > 0 ? (
-                    crew.map(
-                      ({
-                        id,
-                        poster_path,
-                        title,
-                        release_date,
-                        genre_ids,
-                        vote_average,
-                        vote_count,
-                        job,
-                      }) => (
-                        <MovieTile
-                          key={id}
-                          id={id}
-                          posterPath={poster_path}
-                          title={title}
-                          crewJob={isMobile ? undefined : job}
-                          releaseDate={
-                            isMobile ? release_date?.slice(0, 4) : release_date
-                          }
-                          genreIds={genre_ids}
-                          voteAverage={vote_average}
-                          voteCount={vote_count}
-                        />
+                    }
+                  />
+                )}
+
+                {peopleDetails && (
+                  <Article
+                    articleHeader="Movies - crew"
+                    body={
+                      crewLoading ? (
+                        <Loading />
+                      ) : crewError || !crew || crew.length === 0 ? (
+                        <p>No movies available</p>
+                      ) : (
+                        <Content>
+                          {crew.map(
+                            ({
+                              id,
+                              poster_path,
+                              title,
+                              release_date,
+                              genre_ids,
+                              vote_average,
+                              vote_count,
+                              job,
+                            }) => (
+                              <MovieTile
+                                key={id}
+                                id={id}
+                                posterPath={poster_path}
+                                title={title}
+                                crewJob={isMobile ? undefined : job}
+                                releaseDate={
+                                  isMobile
+                                    ? release_date?.slice(0, 4)
+                                    : release_date
+                                }
+                                genreIds={genre_ids}
+                                voteAverage={vote_average}
+                                voteCount={vote_count}
+                              />
+                            )
+                          )}
+                        </Content>
                       )
-                    )
-                  ) : (
-                    <p>No movies available</p>
-                  )}
-                </Content>
-              }
-            />
-          </>
-        }
-      />
-    </Wrapper>
+                    }
+                  />
+                )}
+              </>
+            }
+          />
+        </Wrapper>
+      )}
+    </>
   );
 }
 
