f098e5094b28b690c73ef0d610fd95af69e5ab3a
Donata Deptula <dondeptu@gmail.com>
2025-02-10 00:18:46 +0100

    trim whitespace and compare queries before sending

--- src/common/Navigation/Search/queryParameters.js
+++ src/common/Navigation/Search/queryParameters.js
@@ -21,9 +21,10 @@ export const useUpdateQueryParameter = setSearchQuery => {
     return ((newQuery) => {
         clearTimeout(timeout.current);
         setSearchQuery(newQuery);
+        const trimedQuery = newQuery.trim();
 
-        if (newQuery) {
-            params.set(searchQueryParamName, newQuery);
+        if (trimedQuery) {
+            params.set(searchQueryParamName, trimedQuery);
         } else {
             params.delete(searchQueryParamName);
         }
@@ -35,8 +36,12 @@ export const useUpdateQueryParameter = setSearchQuery => {
             } else if (path.startsWith(`${toPeople()}/`) && path !== toPeople()) {
                 path = toPeople();
             }
+            const currentHistory = `${history.location.pathname}${history.location.search}`;
+            const newHistory = params.toString() ? `${path}?${params.toString()}` : `${path}`;
 
-            history.push(`${path}?${params.toString()}`);
+            if (currentHistory !== newHistory) {
+                history.push(newHistory);
+            };
         }, searchDelay);
     });
 };
