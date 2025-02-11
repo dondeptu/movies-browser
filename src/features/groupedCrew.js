ef53b03e7d4e4444979559efcdcddb8ea4d637c6
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-02-11 17:00:29 +0100

    ad id to groupedCrew

--- src/features/groupedCrew.js
+++ src/features/groupedCrew.js
@@ -1,7 +1,7 @@
 export const groupCrewPeople = (crew) =>
   Object.values(
-    crew.reduce((acc, { name, job, profile_path }) => {
-      acc[name] ??= { name, profile_path, jobs: [] };
+    crew.reduce((acc, { id, name, job, profile_path }) => {
+      acc[name] ??= { id, name, profile_path, jobs: [] };
       acc[name].jobs.push(job);
       return acc;
     }, {})
